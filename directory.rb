912643cc50ae93731c2f32b862ecf3b3892b413d
Joe Burgess <josephburgess@gmail.com>
2022-11-23 12:28:42 +0000

    More refactoring

--- directory.rb
+++ directory.rb
@@ -1,29 +1,15 @@
-# students = [
-#   { name: "Dr. Hannibal Lecter", cohort: :november },
-#   { name: "Darth Vader", cohort: :november },
-#   { name: "Nurse Ratched", cohort: :november },
-#   { name: "Michael Corleone", cohort: :november },
-#   { name: "Alex DeLarge", cohort: :november },
-#   { name: "The Wicked Witch of the West", cohort: :november },
-#   { name: "Terminator", cohort: :november },
-#   { name: "Freddy Krueger", cohort: :november },
-#   { name: "The Joker", cohort: :november },
-#   { name: "Joffrey Baratheon", cohort: :november },
-#   { name: "Norman Bates", cohort: :november },
-# ]
+@students = []
+
 def input_students
   puts "Please enter student names"
   puts "To finish, just hit return twice"
 
-  students = []
-
   name = gets.chomp
   while !name.empty?
-    students << { name: name, cohort: :november }
-    puts "Now we have #{students.count} students"
+    @students << { name: name, cohort: :november }
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  students
 end
 
 def print_header
@@ -31,17 +17,46 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+def print_students_list
+  @students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def print_menu
+  puts "1. Input students"
+  puts "2. Show students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again."
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+interactive_menu

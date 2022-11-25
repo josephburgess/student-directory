e4cac8d1a970a08b86606684c4db0d216c2c44cc
Joe Burgess <josephburgess@gmail.com>
2022-11-25 13:58:41 +0000

    slitting menu into separate file

---
+++ lib/dir_menu.rb
@@ -0,0 +1,23 @@
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "4"
+    load_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again."
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process($stdin.gets.chomp)
+  end
+end
